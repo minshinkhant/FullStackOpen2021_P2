cac8fd3bc5454467d42166ae76877f257acdb4f2
Min Shin Khant <minshinkhant7@gmail.com>
2021-05-11 21:41:28 -0400

    updating b/forms

--- b/forms/src/App.js
+++ b/forms/src/App.js
@@ -4,6 +4,28 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
 
+const [newNote, setNewNote] = useState(
+  'a new note...'
+)
+
+const handleNoteChange = (event) => {
+  console.log(event.target.value)
+  setNewNote(event.target.value)
+}
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random()<0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote("")
+  }
+
 
   return (
     <div>
@@ -13,6 +35,10 @@ const App = (props) => {
           <Note key={note.id} note={note}/>
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
