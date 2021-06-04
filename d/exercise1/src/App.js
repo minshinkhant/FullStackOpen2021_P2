670b101c11a3d04aec9f9877672871843660bfac
macbook <macbook@macbooks-MacBook-Pro.local>
2021-06-03 21:29:36 -0400

    updating d/exercise1/src/services

--- d/exercise1/src/App.js
+++ d/exercise1/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Note from './components/Note';
-
+import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -28,23 +28,24 @@ const App = () => {
 
   const hook = () => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
+  
+    // const eventHandler = (response) => {
+    //   console.log('promise fulfilled')
+    //   setNotes(response.data)
+    // }
+    noteService.getAll().then(initialNotes => {setNotes(initialNotes)})
   }
+
   useEffect(hook, []);
   console.log("render", notes.length, "notes");
 
   const toggleImportanceOf = (id) => {
-    const url =  `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important}
 
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id !== id? note : response.data))
+    noteService.update(id, changedNote)
+    .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id? note : returnedNote))
     })
   }
 
@@ -52,13 +53,13 @@ const App = () => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      date: new Date(),
-      important: Math.random() < 0.5,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
     }
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
         console.log(response)
       })
